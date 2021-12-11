d044bd266a4107d327e140bf9715b97c7538a96d
Aymane Hrouch <jo.aymane@Gmail.com>
2021-12-11 18:55:58 +0000

    update db credentials

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 /* Database credentials */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'projet_uml');
+define('DB_SERVER', 'sql4.freemysqlhosting.net');
+define('DB_USERNAME', 'sql4458139');
+define('DB_PASSWORD', 'p3qvIY9N98');
+define('DB_NAME', 'sql4458139');
  
 /* Attempt to connect to MySQL database */
 try{
